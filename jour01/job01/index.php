64b5bdf4f67737d927fb51e23198a511020d3bf0
thomas-doan <thomas.doan@laplateforme.io>
2021-11-02 10:07:13 +0100

    job1->02 done

---
+++ jour01/job01/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$str = "LaPlateforme";
+echo "$str <br/>";
+
+$str2 = "Vive";
+
+$str3 = "!";
+
+echo "$str2 "."$str "."!";
+echo "<br/>";
+
+$val = "6";
+echo $val;
+
+$val = "4";
+echo "<br/>";
+
+echo $val;
+echo "<br/>";
+
+$myBool = "true";
+echo $myBool;
+echo "<br/>";
+
+$myBool = "false";
+echo $myBool;
+echo "<br/>";
+
+?>
---
+++ jour01/job02/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$ecole1 = "Hello LaPlateforme!";
+$ecole2 = "Hello LaPlateforme!";
+
+echo $ecole1;
+echo "</br>";
+//afficher variable ecole//
+
+echo $ecole2;
+echo "</br>";
+/* afficher variable ecole2; */
+
+?>
