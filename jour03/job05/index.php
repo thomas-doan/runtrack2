0eb7a108edf67a62d68639256d3aae4c3dc40d92
thomas-doan <thomas.doan@laplateforme.io>
2021-11-04 21:14:34 +0100

    jour 03 ok v1

--- jour03/job05/index.php
+++ jour03/job05/index.php
@@ -48,7 +48,7 @@ for ($j = 0; isset($str[$j]); $j++) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tableau primitif</title>
+    <title>Tableau Voyelle/Consonne</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
